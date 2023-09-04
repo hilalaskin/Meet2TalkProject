f63fb7f65fa72d7447a5fb5735be7af377e01ae6
hilalaskin <hilallaskin@gmail.com>
2023-09-04 23:22:24 +0300

    C# ile yazılan alogritma projesi

---
+++ Meet2TalkApp/Meet2TalkApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        // Plateau'nun sağ üst koordinatlarını okuyoruz
+        Console.WriteLine("Plateau'nun boyutlarını giriniz (Örnek: 5 5)");
+        string[] plateauSize = Console.ReadLine().Split();
+        int maxX = int.Parse(plateauSize[0]);
+        int maxY = int.Parse(plateauSize[1]);
+
+        List<Rover> rovers = new List<Rover>();
+
+        Console.WriteLine("Rover'ların başlangıç pozisyonlarını ve talimatlarını giriniz (Örnek: 1 2 N, LMLMLMLMM):");
+        string roverInput;
+
+
+        while (!string.IsNullOrEmpty(roverInput = Console.ReadLine()))
+        {
+            string[] roverData = roverInput.Split();
+            int startX = int.Parse(roverData[0]);
+            int startY = int.Parse(roverData[1]);
+            char startDirection = char.Parse(roverData[2]);
+
+            Rover rover = new Rover(startX, startY, startDirection);
+            string instructions = roverData[3];
+
+            rover.ExecuteInstructions(instructions);
+            rovers.Add(rover);
+        }
+
+        Console.WriteLine("Sonuçlar:");
+        foreach (Rover rover in rovers)
+        {
+            Console.WriteLine(rover);
+        }
+    }
+}
---
+++ Meet2TalkApp/Meet2TalkApp/Rover.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class Rover
+{
+    public int X { get; private set; }
+    public int Y { get; private set; }
+    public char Direction { get; private set; }
+
+    public Rover(int x, int y, char direction)
+    {
+        X = x;
+        Y = y;
+        Direction = direction;
+    }
+
+    public void TurnLeft()
+    {
+        switch (Direction)
+        {
+            case 'N':
+                Direction = 'W';
+                break;
+            case 'W':
+                Direction = 'S';
+                break;
+            case 'S':
+                Direction = 'E';
+                break;
+            case 'E':
+                Direction = 'N';
+                break;
+        }
+    }
+
+    public void TurnRight()
+    {
+        switch (Direction)
+        {
+            case 'N':
+                Direction = 'E';
+                break;
+            case 'E':
+                Direction = 'S';
+                break;
+            case 'S':
+                Direction = 'W';
+                break;
+            case 'W':
+                Direction = 'N';
+                break;
+        }
+    }
+
+    public void Move()
+    {
+        switch (Direction)
+        {
+            case 'N':
+                Y++;
+                break;
+            case 'E':
+                X++;
+                break;
+            case 'S':
+                Y--;
+                break;
+            case 'W':
+                X--;
+                break;
+        }
+    }
+
+    public void ExecuteInstructions(string instructions)
+    {
+        foreach (char instruction in instructions)
+        {
+            switch (instruction)
+            {
+                case 'L':
+                    TurnLeft();
+                    break;
+                case 'R':
+                    TurnRight();
+                    break;
+                case 'M':
+                    Move();
+                    break;
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{X} {Y} {Direction}";
+    }
+}
+
